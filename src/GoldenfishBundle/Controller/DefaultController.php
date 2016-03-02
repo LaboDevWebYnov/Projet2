fef395e2804260c2755eed037e93d566f8c77da0
marjolainerival <rival.marjolaine@gmail.com>
2016-03-02 15:10:53 +0100

    Construction de la page Accueil

---
+++ src/GoldenfishBundle/Controller/DefaultController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace GoldenfishBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class DefaultController extends Controller
+{
+    public function indexAction($name)
+    {
+        return $this->render('GoldenfishBundle:Default:index.html.twig', array('name' => $name));
+    }
+}
---
+++ src/GoldenfishBundle/GoldenfishBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace GoldenfishBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class GoldenfishBundle extends Bundle
+{
+}
