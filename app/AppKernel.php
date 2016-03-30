b503d5aaf8331c86b91e2ccbae2100005c37b55a
Esraka <esra.karakus@ynov.com>
2016-03-30 13:43:52 +0200

    deleted vendor folder

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -18,6 +18,8 @@ class AppKernel extends Kernel
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new AppBundle\AppBundle(),
             new GoldenfishBundle\GoldenfishBundle(),
+            new FOS\UserBundle\FOSUserBundle(),
+            new Goldenfish\UserBundle\UserBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
---
+++ src/Goldenfish/UserBundle/UserBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Goldenfish\UserBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class UserBundle extends Bundle
+{
+}
