99912613bb7f9a9087306c2e8eb46a40dc914fe5
bito <thibault.hamnache@ynov.com>
2016-04-13 16:29:49 +0200

    registration

--- src/Goldenfish/UserBundle/Form/Type/RegistrationFormType.php
+++ src/Goldenfish/UserBundle/Form/Type/RegistrationFormType.php
@@ -8,9 +8,9 @@ use FOS\UserBundle\Form\Type\RegistrationFormType as BaseType;
 class RegistrationFormType extends BaseType
 {
     public function buildForm(FormBuilderInterface $builder, array $options){
-        $builder->add('username','text', array('label' => 'form.username', 'translation_domain' => 'FOSUserBundle'));
-        $builder->add('email','email', array('label' => 'form.email', 'translation_domain' => 'FOSUserBundle'));
-        $builder->add('password','password', array('label' => 'form.password', 'translation_domain' => 'FOSUserBundle'));
+        $builder->add('username','text', array('label' => 'Pseudo', 'translation_domain' => 'FOSUserBundle'));
+        $builder->add('email','email', array('label' => 'Email', 'translation_domain' => 'FOSUserBundle'));
+        $builder->add('password','password', array('label' => 'Mor de passe', 'translation_domain' => 'FOSUserBundle'));
     }
 
     public function getName()
