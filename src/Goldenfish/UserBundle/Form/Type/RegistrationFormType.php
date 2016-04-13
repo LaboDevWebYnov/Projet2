17f0decc3e795288d4fe4b0e8ec693fde7039e56
EtienneT <tienou42@gmail.com>
2016-04-13 14:48:54 +0200

    Register OK

--- src/Goldenfish/UserBundle/Form/Type/RegistrationFormType.php
+++ src/Goldenfish/UserBundle/Form/Type/RegistrationFormType.php
@@ -1,8 +1,11 @@
 <?php
 
-namespace Fos\UserBundle\Form\Type;
+namespace Goldenfish\UserBundle\Form\Type;
 
-class RegisterFormType
+use Symfony\Component\Form\FormBuilderInterface;
+use FOS\UserBundle\Form\Type\RegistrationFormType as BaseType;
+
+class RegistrationFormType extends BaseType
 {
     public function buildForm(FormBuilderInterface $builder, array $options){
         $builder->add('username','text', array('label' => 'form.username', 'translation_domain' => 'FOSUserBundle'));
@@ -10,13 +13,9 @@ class RegisterFormType
         $builder->add('password','password', array('label' => 'form.password', 'translation_domain' => 'FOSUserBundle'));
     }
 
-    public function getParent()
-    {
-        return 'fos_user_registration';
-    }
-
-    public function getBlockPrefix()
+    public function getName()
     {
         return 'app_user_registration';
     }
+    
 }
