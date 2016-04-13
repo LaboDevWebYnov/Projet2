4a57d60b7c0f1b9ca92dc759f63b444f0755ae14
Esra Ka <esra.karakus@ynov.com>
2016-04-13 13:51:35 +0200

    config

--- src/Goldenfish/UserBundle/Controller/SecurityController.php
+++ src/Goldenfish/UserBundle/Controller/SecurityController.php
@@ -1,10 +1,4 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: Esra
- * Date: 12/04/2016
- * Time: 20:05
- */
 
 namespace Goldenfish\UserBundle\Controller;
 
@@ -12,21 +6,30 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Security\Core\SecurityContext;
 
-class SecurityController
+class SecurityController extends Controller
 {
 
     public function loginAction(Request $request)
     {
         if ($this->get('security.context')->isGranted('IS_AUTHENTICATED_REMEMBERED')) {
-            return $this->redirectToRoute('Goldenfish:GoldenfishBundle:App:index.html.twig');
+            return $this->redirectToRoute('UserBundle:Profile:profile.html.twig');
         }
 
         $authenticationUtils = $this->get('security.authentication_utils');
 
-        return $this->render('UserBundle:Security:login.html.twig', array(
+        $csrfToken = $this->container->has('form.csrf_provider')
+            ? $this->container->get('form.csrf_provider')->generateCsrfToken('authenticate')
+            : null;
+
+        $data =  array(
             'last_username' => $authenticationUtils->getLastUsername(),
-            'error'         => $authenticationUtils->getLastAuthenticationError(),
-        ));
+            'csrf_token' => $csrfToken,
+            'error' =>$authenticationUtils-> getLastAuthenticationError()
+        );
+
+        return $this->render('UserBundle:Security:login.html.twig', $data
+        );
+
     }
 
     public function logoutAction(Request $request)
